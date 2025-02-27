a98988a4b8476a7bb39057a742b0fd023a1e59f8
namnt099 <nam.nt0910@gmail.com>
2025-02-27 10:45:28 +0700

    deloy falvor

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,7 @@ import 'package:base/domain/env/env.dart';
 import 'package:base/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<void> main() async {
-  await dotenv.load(fileName: ".env.product");
-  mainApp();
-}
 
 Future<void> mainApp() async {
   WidgetsFlutterBinding.ensureInitialized();
